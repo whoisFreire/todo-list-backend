e4693672a046047f42ce5fbf78b6793adb4befde
Leonardo Freire <programador.leonardofreire@gmail.com>
2023-01-17 15:07:11 -0300

    feat(DatabaseModule): create user and todo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './modules/database/database.module';
+import { UserModule } from './modules/user/user.module';
+import { TodoModule } from './modules/todo/todo.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UserModule, TodoModule],
   controllers: [],
   providers: [],
 })
--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { TodoRepository } from 'src/application/repositories/todo-repository';
+import { UserRepository } from 'src/application/repositories/user-repository';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaTodosRepository } from './prisma/repositories/prisma-todos-repository';
+import { PrismaUsersRepository } from './prisma/repositories/prisma-users-repository';
 
 @Module({
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: UserRepository,
+      useClass: PrismaUsersRepository,
+    },
+    {
+      provide: TodoRepository,
+      useClass: PrismaTodosRepository,
+    },
+  ],
+  exports: [UserRepository, TodoRepository],
 })
 export class DatabaseModule {}
